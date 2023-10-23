d618d0953c9c6058bb7250fda669101c4bdd9afd
brkuvezdic <branimirkuvezdic@gmail.com>
2023-10-23 17:27:53 +0200

    MongoDB connection + DB security

--- api/index.js
+++ api/index.js
@@ -3,10 +3,17 @@ import mongoose, { mongo } from "mongoose";
 import dotenv from "dotenv";
 dotenv.config();
 
-mongoose.connect(process.env.MONGO);
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 const app = express();
 
 app.listen(3000, () => {
-  console.log("Server is running on port 3000!!");
+  console.log("Server is running on port 3000");
 });
