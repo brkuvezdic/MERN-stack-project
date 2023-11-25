ee3000be2750f4b6f99ee5ce51bca144054488ab
brkuvezdic <branimirkuvezdic@gmail.com>
2023-11-25 14:44:59 +0100

    Added Google OAuth

---
+++ client/src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBV_EYvKzyrk6HtRqo4EsvYRvW9R0yH3xY",
+  authDomain: "mern-stack-project-cdcce.firebaseapp.com",
+  projectId: "mern-stack-project-cdcce",
+  storageBucket: "mern-stack-project-cdcce.appspot.com",
+  messagingSenderId: "506917316585",
+  appId: "1:506917316585:web:cb25773e61aa1827934d78",
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
